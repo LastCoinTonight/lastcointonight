9673262a628f7303c59ad7b72cb27379be48cbdc
leobauza <leonardobauza@gmail.com>
2017-07-15 02:05:07 -0400

    move folders and get images working on md files

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,28 +9,28 @@ module.exports = {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
-          `gatsby-remark-copy-linked-files`
+          `gatsby-remark-copy-linked-files`,
           // @TODO add when fixed...
-          // {
-          //   resolve: `gatsby-remark-images`,
-          //   options: {
-          //     // It's important to specify the maxWidth (in pixels) of
-          //     // the content container as this plugin uses this as the
-          //     // base for generating different widths of each image.
-          //     maxWidth: 590,
-          //     // Remove the default behavior of adding a link to each
-          //     // image.
-          //     linkImagesToOriginal: false,
-          //   },
-          // },
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 590,
+              // Remove the default behavior of adding a link to each
+              // image.
+              linkImagesToOriginal: false,
+            },
+          },
         ]
       }
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `posts`,
-        path: `${__dirname}/src/content`,
+        name: `pages`,
+        path: `${__dirname}/src/pages`,
       },
     },
   ],
